949d1ed0077c86e56d1030b8107a24458bf321e6
ismailonur <ismailonur15@gmail.com>
2020-09-22 14:22:15 +0300

    DOTween plugini import edildi. MenuLevel oluşturuldu.

---
+++ Assets/Scripts/MenuLevel/MenuManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject hakkimdaPanel;
+
+    bool panelAcikmi;
+
+    private void Start()
+    {
+        panelAcikmi = false;
+    }
+
+    public void OyunaBasla()
+    {
+        SceneManager.LoadScene("GameLevel");
+    }
+
+    public void HakkimdaPaneliniAc()
+    {
+        if (!panelAcikmi)
+        {
+            hakkimdaPanel.GetComponent<CanvasGroup>().DOFade(1, 0.5f);
+        }
+        else
+        {
+            hakkimdaPanel.GetComponent<CanvasGroup>().DOFade(0, 0.5f);
+        }
+
+        panelAcikmi = !panelAcikmi;
+    }
+
+    public void OyundanCik()
+    {
+        Application.Quit();
+    }
+}
